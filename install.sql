25146af9ed436fbcff2d5f63eec6d7d1c0068c63
Ivan Davis <techbyivan@users.noreply.github.com>
2020-07-24 07:39:15 -0400

    Updates...

--- install.sql
+++ install.sql
@@ -4,7 +4,19 @@ CREATE TABLE tasks (
     is_completed BOOLEAN DEFAULT FALSE 
 );
 
-INSERT INTO tasks (title) VALUES ('Buy milk');
-INSERT INTO tasks (title) VALUES ('Sweep the dog');
-INSERT INTO tasks (title) VALUES ('Milk the house');
-INSERT INTO tasks (title) VALUES ('Feed The store');
+INSERT INTO tasks (title) VALUES ('Buy');
+INSERT INTO tasks (title) VALUES ('Buy Crafts');
+INSERT INTO tasks (title) VALUES ('Buy Furniture');
+INSERT INTO tasks (title) VALUES ('Buy Ruler');
+INSERT INTO tasks (title) VALUES ('Buy Pens');
+INSERT INTO tasks (title) VALUES ('Buy Television');
+INSERT INTO tasks (title) VALUES ('Buy Calulator');
+INSERT INTO tasks (title) VALUES ('Buy Kitchen Table');
+INSERT INTO tasks (title) VALUES ('Buy Car');
+INSERT INTO tasks (title) VALUES ('Buy Coats');
+INSERT INTO tasks (title) VALUES ('Buy Headphones');
+INSERT INTO tasks (title) VALUES ('Buy Picture Frames');
+INSERT INTO tasks (title) VALUES ('Buy Computer');
+INSERT INTO tasks (title) VALUES ('Buy Mobile Phones');
+INSERT INTO tasks (title) VALUES ('Buy Car Alarm');
+INSERT INTO tasks (title) VALUES ('Buy Shoes');
